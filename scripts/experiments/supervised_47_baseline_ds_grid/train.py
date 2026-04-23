702d3ac295cc6725c2797ca1f3247a6022bfa317
Chester Henry Charlton <henrycharlton1@gmail.com>
2026-04-23 11:23:46 +0200

    add a guard for preventing double worker spawns

--- scripts/experiments/supervised_47_baseline_ds_grid/train.py
+++ scripts/experiments/supervised_47_baseline_ds_grid/train.py
@@ -2,4 +2,6 @@
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
 from scripts.ds_grid import main
-main(sys.argv[1])
+
+if __name__ == "__main__":
+    main(sys.argv[1])
